87b87e2dfe8d2491218a89c3ed587930e493dc7b
David Vargas <dvargas92495@gmail.com>
2020-09-12 18:58:29 -0400

    Querying local block chain with great success

--- client/src/pages/FlightPage.js
+++ client/src/pages/FlightPage.js
@@ -1,67 +1,112 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { redirect } from './utils';
-import { withRouter } from 'react-router-dom';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { redirect } from "./utils";
+import { withRouter } from "react-router-dom";
+import Web3 from "web3";
+import contractFactory from "../contracts/FlightContractFactory.json";
+import TruffleContract from "@truffle/contract";
+
+const web3 = window.ethereum
+  ? new Web3(window.ethereum)
+  : new Web3("ws://localhost:7545");
+const flightContractFactory = TruffleContract(contractFactory);
+flightContractFactory.setProvider(web3.currentProvider);
 
 class FlightPage extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      price: props.flight.price
+      price: props.flight.price,
+      address: "",
+      contractAddress: "",
+      factoryInstance: null,
     };
     this.mounted = true;
   }
 
   componentDidMount = () => {
     this.loadCovidCases();
-  }
+    window.ethereum
+      .enable()
+      .then(() => web3.eth.getAccounts())
+      .then((accounts) => {
+        this.setAccount(accounts[0]);
+        return flightContractFactory.deployed();
+      })
+      .then((factoryInstance) => this.setFactoryInstance(factoryInstance));
+  };
 
   componentWillUnmount = () => {
     this.mounted = false;
-  }
+  };
 
-  onError = error => {
+  onError = (error) => {
     this.setState({ error: error.message });
-  }
+  };
 
   loadCovidCases = async () => {
     this.setState({ isLoading: true });
     const covidCases = 20; // Load covid cases using this.props.flight.endLoc
     if (this.mounted) {
       this.setState({ isLoading: false });
-      if (covidCases) this.setState({ covidCases, desiredCovidCases: covidCases });
+      if (covidCases)
+        this.setState({ covidCases, desiredCovidCases: covidCases });
     }
-  }
+  };
 
-  onChange = async event => {
+  onChange = async (event) => {
     const field = event.target.name;
     const value = Number(event.target.value);
     this.setState({ [field]: value, isLoading: true });
-    const price = 240; // Get price based on this.props.flightId and desired covid cases 
+    const price = 240; // Get price based on this.props.flightId and desired covid cases
     if (this.mounted) {
       this.setState({ isLoading: false });
       if (price) this.setState({ price });
     }
-  }
+  };
 
-  onSave = () => {
+  setAccount = (address) => this.setState({ address });
+
+  setFactoryInstance = (factoryInstance) => this.setState({ factoryInstance });
+
+  onSave = async () => {
     this.setState({ isSaving: true });
-    const contract = { 
-      flightId: this.props.flightId, 
+    const contract = {
+      flightId: this.props.flightId,
       desiredCovidCases: this.state.desiredCovidCases,
-      price: this.state.price
+      price: this.state.price,
     }; // Submit contract based on this.props.flightId and desired covid cases
-    if (this.mounted) {
-      this.setState({ isSaving: false });
-      if (contract) this.setState({ contract });
+    try {
+      window.myInstance = this.state.factoryInstance;
+      const contractAddress = await this.state.factoryInstance.newFlightContract(
+        this.state.address,
+        contract.price,
+        contract.flightId,
+        contract.desiredCovidCases,
+        { from: this.state.address }
+      );
+      if (this.mounted) {
+        this.setState({
+          isSaving: false,
+          contract,
+          contractAddress: contractAddress.tx,
+        });
+      }
+    } catch (e) {
+      if (e.code !== 4001) {
+        console.error(e.message);
+      }
+      if (this.mounted) {
+        this.setState({ isSaving: false });
+      }
     }
-  }
+  };
 
-  redirect = link => () => {
+  redirect = (link) => () => {
     this.props.actions.redirect(link);
-  }
+  };
 
   render() {
     const flight = this.props.flight;
@@ -70,7 +115,8 @@ class FlightPage extends Component {
     const contract = this.state.contract;
     return (
       <div className="FlightPage">
-        <button onClick={this.redirect('/')}>Back</button>
+        {this.state.address && <div>Signed in as {this.state.address}</div>}
+        <button onClick={this.redirect("/")}>Back</button>
         <br />
         <br />
         <div>From: {flight.startLoc}</div>
@@ -83,12 +129,15 @@ class FlightPage extends Component {
         {contract ? (
           <div>
             Success! If the number of Covid cases in {flight.endLoc}
-            are below {contract.desiredCovidCases} a week before {flight.endTime}
-            you will be flying there for only ${contract.price}!
+            are below {contract.desiredCovidCases} a week before{" "}
+            {flight.endTime}
+            you will be flying there for only ${contract.price}! The contract is
+            stored at {this.state.contractAddress}!
           </div>
         ) : (
           <div>
-            Current new cases per day in {flight.endLoc}: {this.state.covidCases}
+            Current new cases per day in {flight.endLoc}:{" "}
+            {this.state.covidCases}
             <br />
             <input
               type="number"
@@ -102,10 +151,7 @@ class FlightPage extends Component {
             <br />
             You pay: ${this.state.price}
             <br />
-            <button 
-              onClick={this.onSave}
-              disabled={isLoading || isSaving}
-            >
+            <button onClick={this.onSave} disabled={isLoading || isSaving}>
               Submit
             </button>
           </div>
@@ -118,21 +164,23 @@ class FlightPage extends Component {
 FlightPage.propTypes = {
   actions: PropTypes.object.isRequired,
   flightId: PropTypes.string.isRequired,
-  flight: PropTypes.object.isRequired
+  flight: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state, ownProps) {
   const flightId = ownProps.match.params.flightId;
   return {
     flightId,
-    flight: state.flights.find(f => f.id === flightId)
+    flight: state.flights.find((f) => f.id === flightId),
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators({ redirect }, dispatch)
+    actions: bindActionCreators({ redirect }, dispatch),
   };
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(FlightPage));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(FlightPage)
+);
--- client/src/pages/FlightsPage.js
+++ client/src/pages/FlightsPage.js
@@ -1,33 +1,68 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { redirect } from './utils';
-import { withRouter } from 'react-router-dom';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { redirect } from "./utils";
+import { withRouter } from "react-router-dom";
+import Web3 from "web3";
+import contractFactory from "../contracts/FlightContractFactory.json";
+import TruffleContract from "@truffle/contract";
+
+const web3 = window.ethereum
+  ? new Web3(window.ethereum)
+  : new Web3("ws://localhost:7545");
+const flightContractFactory = TruffleContract(contractFactory);
+flightContractFactory.setProvider(web3.currentProvider);
 
 class FlightsPage extends Component {
-  redirect = link => () => {
-    this.props.actions.redirect(link);
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: "",
+      contractCount: 0,
+    };
   }
+
+  componentDidMount = () => {
+    window.ethereum
+      .enable()
+      .then(() => web3.eth.getAccounts())
+      .then((accounts) => {
+        this.setAccount(accounts[0]);
+        return flightContractFactory.deployed();
+      })
+      .then((factoryInstance) => factoryInstance.getContractCount())
+      .then((count) => this.setContractCount(count.toNumber()));
+  };
+
+  setAccount = (address) => this.setState({ address });
+
+  setContractCount = (contractCount) => this.setState({ contractCount });
+
+  redirect = (link) => () => {
+    this.props.actions.redirect(link);
+  };
 
   render() {
     const flights = this.props.flights;
     return (
       <div className="FlightsPage">
+        {this.state.address && <div>Signed in as {this.state.address}</div>}
         <ul>
-        {flights.map(f => (
-          <li
-            key={f.id}
-            onClick={this.redirect(`/${f.id}`)}
-          >
-            From: {f.startLoc}
-            To: {f.endLoc}
-            Departing: {f.startTime}
-            Arriving: {f.endTime}
-            Price: ${f.price}
-          </li>
-        ))}
+          {flights.map((f) => (
+            <li key={f.id} onClick={this.redirect(`/${f.id}`)}>
+              From: {f.startLoc}
+              To: {f.endLoc}
+              Departing: {f.startTime}
+              Arriving: {f.endTime}
+              Price: ${f.price}
+            </li>
+          ))}
         </ul>
+        <div>
+          Total number of flight contracts:{" "}
+          {this.state.contractCount}
+        </div>
       </div>
     );
   }
@@ -35,19 +70,21 @@ class FlightsPage extends Component {
 
 FlightsPage.propTypes = {
   actions: PropTypes.object.isRequired,
-  flights: PropTypes.array.isRequired
+  flights: PropTypes.array.isRequired,
 };
 
 function mapStateToProps(state, ownProps) {
   return {
-    flights: state.flights
+    flights: state.flights,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators({ redirect }, dispatch)
+    actions: bindActionCreators({ redirect }, dispatch),
   };
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(FlightsPage));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(FlightsPage)
+);
