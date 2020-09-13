bcb66a2310772b854ac6c22ea22a012e00a4c610
David Vargas <dvargas92495@gmail.com>
2020-09-13 19:15:50 -0400

    TF Deploy Site

---
+++ aws.tf
@@ -0,0 +1,49 @@
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "VargasArts"
+        workspaces {
+            prefix = "wings"
+        }
+    }
+}
+
+variable "secret" {
+    type = string
+}
+
+variable "github_token" {
+    type = string
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "aws-static-site" {
+    source  = "dvargas92495/static-site/aws"
+    version = "1.0.0"
+
+    domain = "wings.davidvargas.me"
+    secret = var.secret
+    tags = {
+        Application = "Wings"
+    }
+}
+
+provider "github" {
+    owner = "dvargas92495"
+    token = var.github_token
+}
+
+resource "github_actions_secret" "deploy_aws_access_key" {
+  repository       = "wings"
+  secret_name      = "DEPLOY_AWS_ACCESS_KEY_ID"
+  plaintext_value  = module.aws-static-site.deploy-id
+}
+
+resource "github_actions_secret" "deploy_aws_access_secret" {
+  repository       = "wings"
+  secret_name      = "DEPLOY_AWS_SECRET_ACCESS_KEY"
+  plaintext_value  = module.aws-static-site.deploy-secret
+}
