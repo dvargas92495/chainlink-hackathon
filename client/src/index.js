c130e18b669367c6922493ae9a51f8601f7c37f3
David Vargas <dvargas92495@gmail.com>
2020-09-27 15:03:15 -0400

    Trigger deploy

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
-
 import store, { history } from './store/configureStore';
 
 render(
